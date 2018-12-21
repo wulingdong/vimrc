de5c5dfa57f29abdd986a6bfe5bf9e8cf9241bf1
lingodng wu <lingdongwu@dinoplus.ai>
2018-12-21 15:32:36 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#########################################################################
+# File Name: install.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: 2018年12月21日 星期五 15时22分32秒
+#########################################################################
+#!/bin/bash
+cur_dir=`pwd`
+unlink ~/.vimrc
+unlink ~/.vim
+
+ln -s $cur_dir/vimrc ~/.vimrc
+ln -s $cur_dir ~/.vim
+
+if [ ! -d $cur_dir/bundle  ];then
+	mkdir $cur_dir/bundle
+    git clone https://github.com/VundleVim/Vundle.vim.git $cur_dir/bundle
+fi 
